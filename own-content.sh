44feb0fc99304e6b7816ca8c27edd8e20223a883
Rajesh Alli <arajesh.alli@gmail.com>
2022-09-15 10:11:08 +0530

    a

--- own-content.sh
+++ own-content.sh
@@ -3,10 +3,18 @@
 source common.sh
 MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
+Print "Download Html Pages"
+curl -s -L -o /tmp/master.zip "https://github.com/microsoft/project-html-website/archive/refs/heads/master.zip" &>>$LOG
+Stat $?
+
 Print "Remove Old Html Pages"
 rm -rf /usr/share/nginx/html/* &>>$LOG
 Stat $?
 
-Print "copy own content"
-hello rajesh &>>$LOG
+Print "Extract Frontend Archive"
+unzip -o -d /tmp /tmp/master.zip &>>$LOG
 Stat $?
+
+Print "Copy files to Nginx path"
+mv /tmp/main/static/* /usr/share/nginx/html/. &>>$LOG
+Stat $??
