1a2fadef07cef91c256f00d6603e1d56eda06776
Rajesh Alli <arajesh.alli@gmail.com>
2022-09-15 09:40:55 +0530

    common.sh

---
+++ common.sh
@@ -0,0 +1,21 @@
+Print() {
+  LSPACE=$(echo $1 | awk '{print length}')
+  SPACE=$(($MSPACE-$LSPACE))
+  SPACES=""
+  while [ $SPACE -gt 0 ]; do
+    SPACES="$SPACES$(echo ' ')"
+    SPACE=$(($SPACE-1))
+  done
+  echo -n -e "\e[1m$1${SPACES}\e[0m  ... "
+  echo -e "\n\e[36m======================== $1 ========================\e[0m" >>$LOG
+}
+
+Stat() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo -e "\e[1;33mScript Failed and check the detailed log in $LOG file\e[0m"
+    exit 1
+  fi
+}
