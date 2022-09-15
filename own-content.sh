2e9f08fcb9b2d3215589820a386f21a1f11b72ec
Rajesh Alli <arajesh.alli@gmail.com>
2022-09-15 10:05:23 +0530

    a

--- own-content.sh
+++ own-content.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source components/common.sh
+source common.sh
 MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 Print "Remove Old Html Pages"
